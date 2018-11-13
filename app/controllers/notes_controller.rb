2c2dea6487536b8bfd6a70ec4434081d77f9a0e8
Emilio <em.francomartinez@gmail.com>
2018-11-13 08:20:53 -0700

    more updates

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,6 +1,6 @@
 class NotesController < ApplicationController
   def index
-    @notes = Notes.all
+    @notes = Note.all
   end
 
   def show
@@ -13,4 +13,32 @@ class NotesController < ApplicationController
   def new
     @note = Note.new
   end
+
+  def create
+    @note = Note.new(notes_params)
+
+    if @note.save
+      redirect_to notes_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @note = Note.find(params[:id])
+  
+    if @note.update(note_params)
+      redirect_to notes_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Note.find(params[:id]).destroy
+    redirect_to notes_path
+  end
+
+  def notes_params
+    params.require(:note).permit(:title, :author, :body)
 end
