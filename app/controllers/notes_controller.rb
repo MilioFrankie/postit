c36daa67de7ae591277c336ae23da2269be66326
Emilio <em.francomartinez@gmail.com>
2018-11-12 17:30:13 -0700

    updated controllers

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,13 +1,16 @@
 class NotesController < ApplicationController
   def index
+    @notes = Notes.all
   end
 
   def show
+    @note = Note.find(params[:id])
   end
 
   def edit
   end
 
   def new
+    @note = Note.new
   end
 end
